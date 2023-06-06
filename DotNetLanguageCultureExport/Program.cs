0ce8a304f36bc197c5dd99ea87787d768c160d8c
Tim Trott <6177944+azuliadesigns@users.noreply.github.com>
2023-06-06 12:11:30 +0100

    Initial commit

---
+++ DotNetLanguageCultureExport/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Text;
+using System.Web;
+
+CultureInfo[] cinfo = CultureInfo.GetCultures(CultureTypes.AllCultures & ~CultureTypes.NeutralCultures);
+StringBuilder data = new StringBuilder();
+data.AppendLine("ID,\"Country\",\"Language\",\"TwoLetterISORegionName\",\"ThreeLetterISORegionName\",\"TwoLetterISOLanguageName\",\"ThreeLetterISOLanguageName\",\"CultureInfo\"");
+int c = 1;
+
+foreach (CultureInfo cul in cinfo)
+{
+    try
+    {
+        RegionInfo ri = new RegionInfo(cul.Name);   
+
+        string Country = HttpUtility.HtmlEncode(ri.EnglishName);
+        string Language = HttpUtility.HtmlEncode(cul.EnglishName);
+        string Native = HttpUtility.HtmlEncode(cul.NativeName);
+        string TwoLetterISORegionName = ri.TwoLetterISORegionName;
+        string ThreeLetterISORegionName = ri.ThreeLetterISORegionName;
+        string TwoLetterISOLanguageName = cul.TwoLetterISOLanguageName;
+        string ThreeLetterISOLanguageName = cul.ThreeLetterISOLanguageName;
+        string CultureInfo = cul.Name;
+
+        data.AppendLine(string.Format("{0},\"{1}\",\"{2}\",\"{3}\",\"{4}\",\"{5}\",\"{6}\",\"{7}\"", c, Country, Language, TwoLetterISORegionName, ThreeLetterISORegionName, TwoLetterISOLanguageName, ThreeLetterISOLanguageName, CultureInfo));
+        c++;
+    }
+    catch
+    {
+        continue;
+    }
+}
+
+File.WriteAllText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "DotNetLanguagesAndCultures.csv"), data.ToString());
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DotNetLanguageCultureExport
-A simple tool which will export a CSV list for each installed country and language CultureInfo record
+A simple tool written in Visual Studio 2022 which will export a CSV list for each installed country and language CultureInfo record for .Net 6.0
